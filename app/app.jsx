c7aa6d689343965852e10ee65397b37fd0bf37ee
dung <dungvatoi12>
2017-04-16 01:06:49 +0700

    genera struct project

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Provider} = require('react-redux');
+
+
+
+// Load foundation
+// add all features of foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <p>Hello React Redux</p>,
+    document.getElementById("app")
+)
